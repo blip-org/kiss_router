540cf437e93beef07285329a1164e8c76cf4654a
campfire5 <barbaqadzedato3@gmail.com>
2023-04-07 00:25:06 +0400

    docs: base guard class

--- lib/src/models/base_guard.dart
+++ lib/src/models/base_guard.dart
@@ -1,3 +1,9 @@
+/// Base class for defining guards of the route.
+///
+/// If the concrete implementation of the [BaseGuard]'s [call()] method
+/// returns true, it means that guard rules has been passed and
+/// the route that integrates this same guard, will be allowed
+/// to be navigated,
 abstract class BaseGuard {
   bool call();
 }
